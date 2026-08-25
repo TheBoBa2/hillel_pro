85c4506df3f34f86c73ae2f4a09848c62161d6e5
TheBoBa2 <glooplootloo@gmail.com>
2026-08-25 18:24:11 +0300

    added lists 08 and 09

---
+++ 03/08_dictionaries.py
@@ -0,0 +1,12 @@
+﻿def dictionary_keys(dictionary):
+    for key in dictionary:
+        print(key)
+
+my_dict = {
+    "animal": "Cat",
+    "name": "Zeus",
+    "age": 13,
+    "breed": "British"
+}
+
+dictionary_keys(my_dict)
---
+++ 03/09_dictionaries.py
@@ -0,0 +1,15 @@
+﻿def join_dictionaries(first_dict, second_dict):
+    result = {**first_dict, **second_dict}
+    return result
+
+first_input = {
+    "animal": "Cat",
+    "name": "Zeus"
+}
+
+second_input = {
+    "age": 13,
+    "breed": "British"
+}
+
+print("Результат:", join_dictionaries(first_input, second_input))
