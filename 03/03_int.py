de4ba7fcaeac9b9c9dc34ec6bb3931ff37eab598
TheBoBa2 <glooplootloo@gmail.com>
2026-08-25 17:19:04 +0300

    added int 03, 04 and 05

---
+++ 03/03_int.py
@@ -0,0 +1,5 @@
+﻿def square(number):
+    return number * number
+
+input_number = int(input())
+print("Квадрат числа:", square(input_number))
---
+++ 03/04_int.py
@@ -0,0 +1,6 @@
+﻿def sum_numbers(a, b):
+    return a + b
+
+first_input = int(input())
+second_input = int(input())
+print("сума: ", sum_numbers(first_input, second_input))
