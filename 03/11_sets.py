fa1be11b31f6de805ee441b6a20611fe91ab9693
TheBoBa2 <glooplootloo@gmail.com>
2026-08-25 18:35:29 +0300

    added sets 10 and 11

---
+++ 03/11_sets.py
@@ -0,0 +1,9 @@
+﻿def is_subset(first_set, second_set):
+    result = first_set.issubset(second_set)
+    return result
+
+
+first_input = set(map(int, input().split()))
+second_input = set(map(int, input().split()))
+
+print("Результат: ", is_subset(first_input, second_input))
