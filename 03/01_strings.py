bfc7943071c2cef1e059b9e34fef0d33ea66a5c8
TheBoBa2 <glooplootloo@gmail.com>
2026-08-25 16:58:57 +0300

    added string 01 and 02

---
+++ 03/01_strings.py
@@ -0,0 +1,5 @@
+﻿def string_length(input_string):
+    return len(input_string)
+
+input_str = input()
+print("Довжина строки: ", string_length(input_str))
---
+++ 03/02_strings.py
@@ -0,0 +1,6 @@
+﻿def string_combiner(string_a, string_b):
+    return string_a + string_b
+
+first_input = input()
+second_input = input()
+print("Ітоговий рядок: ", string_combiner(first_input, second_input))
