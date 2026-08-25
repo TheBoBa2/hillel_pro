5aafe6ed88cab6f7dbb45aae5ab6387c981ff029
TheBoBa2 <glooplootloo@gmail.com>
2026-08-25 18:40:47 +0300

    added conditional expressions 12 and 13

---
+++ 03/12_conditional expressions.py
@@ -0,0 +1,9 @@
+﻿def even_or_odd(number):
+    if number % 2 == 0:
+        print("Парне")
+    else:
+        print("Непарне")
+
+
+input_num = int(input())
+even_or_odd(input_num)
---
+++ 03/13_conditional expressions.py
@@ -0,0 +1,10 @@
+﻿def even_numbers(numbers):
+    result = []
+    for number in numbers:
+        if number % 2 == 0:
+            result.append(number)
+    return result
+
+
+numbers = list(map(int, input().split()))
+print("Результат: ", even_numbers(numbers))
